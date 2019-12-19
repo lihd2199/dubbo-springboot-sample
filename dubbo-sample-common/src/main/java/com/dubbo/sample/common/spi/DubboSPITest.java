eac2ba9abeaf60bd284caa291a64b97f1d75c1a8
hd_li <hd_li@ctrip.com>
2019-12-19 11:06:58 +0800

    Dubbo SPI

--- dubbo-sample-common/src/main/java/com/dubbo/sample/common/spi/DubboSPITest.java
+++ dubbo-sample-common/src/main/java/com/dubbo/sample/common/spi/DubboSPITest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 public class DubboSPITest {
 
     @Test
-    public void sayHello() throws Exception {
+    public void sayHello() {
         ExtensionLoader<Robot> extensionLoader =
                 ExtensionLoader.getExtensionLoader(Robot.class);
         Robot optimusPrime = extensionLoader.getExtension("optimusPrime");
